91bee0e36b79374fcc060c6f45efc2d6b0dd119d
EL96cpp <el96cpp@gmail.com>
2024-08-05 20:22:48 +0300

    Created sqlalchemy async database connection, added db_sqlite3 to .gitignore

---
+++ recipes_api/core/models/__init__.py
@@ -0,0 +1,10 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    "Categroy"
+)
+
+from .base import Base
+from .db_helper import DatabaseHelper, db_helper
+from .category import Category
--- recipes_api/main.py
+++ recipes_api/main.py
@@ -1,11 +1,20 @@
 import uvicorn
+from contextlib import asynccontextmanager
+from core.models import Base, db_helper
 from fastapi import FastAPI
 from profile.views import router as profile_router
 from users.views import router as users_router
 from recipes.views import router as recipes_router
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(profile_router, tags=["Profile"])
 app.include_router(users_router, tags=["Users"])
 app.include_router(recipes_router, tags=["Recipes"])
