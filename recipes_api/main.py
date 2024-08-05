0bd49cbd846deaec74ebd697d13c49ebf1f77b53
EL96cpp <el96cpp@gmail.com>
2024-08-05 17:08:06 +0300

    Included profile, users and recipes routers in main.py file, added corresponding tags

--- recipes_api/main.py
+++ recipes_api/main.py
@@ -1,8 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
+from profile.views import router as profile_router
+from users.views import router as users_router
+from recipes.views import router as recipes_router
 
 
 app = FastAPI()
+app.include_router(profile_router, tags=["Profile"])
+app.include_router(users_router, tags=["Users"])
+app.include_router(recipes_router, tags=["Recipes"])
 
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True)
