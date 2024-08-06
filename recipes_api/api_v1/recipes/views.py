98281bca9da88e73a0bb975c791943d9b6068e07
EL96cpp <el96cpp@gmail.com>
2024-08-06 18:39:39 +0300

    Changed status code for successful recipe post request to 201

--- recipes_api/api_v1/recipes/views.py
+++ recipes_api/api_v1/recipes/views.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from . import crud 
-from .schemas import Recipy, RecipyCreate
+from .schemas import Recipy, RecipyCreate, RecipyUpdate, RecipyUpdatePartial
 from .dependencies import recipy_by_id
 from core.models import db_helper
 
@@ -18,11 +18,38 @@ async def get_recipy(recipy: Recipy = Depends(recipy_by_id)):
     return recipy 
 
 
-@router.post("", response_model=Recipy)
+@router.post("", response_model=Recipy, status_code=status.HTTP_201_CREATED)
 async def create_recipy(recipy_in: RecipyCreate, session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     return await crud.create_recipy(session=session, recipy_in=recipy_in)
 
 
 @router.put("/{recipy_id}", response_model=Recipy)
-async def update_recipy():
+async def update_recipy(
+    recipy_update: RecipyUpdate,
+    recipy: Recipy = Depends(recipy_by_id), 
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    return await crud.update_recipy(
+        session=session,
+        recipy=recipy,
+        recipy_update=recipy_update
+    )
+
+
+@router.patch("/{recipy_id}", response_model=Recipy)
+async def update_recipy_partial(
+    recipy_update: RecipyUpdatePartial,
+    recipy: Recipy = Depends(recipy_by_id), 
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+):
+    return await crud.update_recipy(
+        session=session,
+        recipy=recipy,
+        recipy_update=recipy_update,
+        partial=True
+    )
+
+
+@router.delete("/{recipy_id}")
+async def delete_recipy(recipy: Recipy = Depends(recipy_by_id), session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     pass
