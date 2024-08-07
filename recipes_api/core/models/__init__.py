df74b66e43bd2245b1803cfc498a4b403aff857a
EL96cpp <el96cpp@gmail.com>
2024-08-07 12:08:39 +0300

    Created User model, added migration for to create users table

--- recipes_api/core/models/__init__.py
+++ recipes_api/core/models/__init__.py
@@ -2,9 +2,13 @@ __all__ = (
     "Base",
     "DatabaseHelper",
     "db_helper",
-    "Categroy"
+    "Categroy",
+    "Product",
+    "Recipy",
+    "User"
 )
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
-from .recipy import Category
+from .recipy import Category, Product, Recipy
+from .users import User
