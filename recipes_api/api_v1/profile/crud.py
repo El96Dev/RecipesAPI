10cbd09c7d37cf7267eb5611b7cf19d50c25283c
EL96cpp <el96cpp@gmail.com>
2024-08-14 20:45:25 +0300

    Added GET view to get recipes created by user in profile router

--- recipes_api/api_v1/profile/crud.py
+++ recipes_api/api_v1/profile/crud.py
@@ -8,9 +8,21 @@ from core.models.user import User
 from core.models.like import Like
 
 
-
 async def get_user_likes(session: AsyncSession, user_id: int):
     stmt = select(User).options(selectinload(User.likes)).where(User.id==user_id)
     result = await session.execute(stmt)
     user = result.scalars().one_or_none()
     return user.likes
+
+
+async def get_user_created_recipes(session: AsyncSession, user_id: int):
+    author_stmt = select(User.email).where(User.id==user_id)
+    author_result = await session.execute(author_stmt)
+    author = author_result.scalars().one_or_none()
+
+    if not author:
+        raise HTTPException(status_code=404, detail=f"User with id {user_id} not found!")
+
+    recipes_stmt = select(Recipy).where(Recipy.author==author)
+    recipes_result = await session.execute(recipes_stmt)
+    return recipes_result.scalars().all()
--- recipes_api/api_v1/profile/views.py
+++ recipes_api/api_v1/profile/views.py
@@ -13,3 +13,9 @@ router = APIRouter(tags=["Profile"])
 async def get_user_likes(user_id: int, 
                          session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
     return await crud.get_user_likes(session, user_id)
+
+
+@router.get("/{user_id}/created_recipes")
+async def get_user_created_recipes(user_id: int,
+                                   session: AsyncSession = Depends(db_helper.scoped_session_dependency)):
+    return await crud.get_user_created_recipes(session, user_id)
