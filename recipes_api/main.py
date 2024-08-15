219811370d61d7295acdcf8ff62464c2d39176be
EL96cpp <el96cpp@gmail.com>
2024-08-15 11:02:48 +0300

    Removed unused python packages 'users', 'profile' and 'recipes'

--- recipes_api/main.py
+++ recipes_api/main.py
@@ -3,9 +3,6 @@ from contextlib import asynccontextmanager
 from core.models import Base, db_helper
 from fastapi import FastAPI
 from core.config import settings
-from profile.views import router as profile_router
-from users.views import router as users_router
-from recipes.views import router as recipes_router
 from api_v1 import router as router_v1
 
 
