c37ba034d913d7993bc82b4c80ba19a50df642e9
El96Dev <el96cpp@gmail.com>
2025-02-07 06:39:53 +0300

    Add script create_admin.py to create admin user

---
+++ recipes_api/create_admin.py
@@ -0,0 +1,19 @@
+from fastapi_users.password import PasswordHelper
+
+from core.models import User, db_helper
+
+
+async def create_admin_user(username: str, email: str, password: str):
+    async for session in db_helper.scoped_session_dependency():
+        password_helper = PasswordHelper()
+        hashed_password = password_helper.hash(password)
+        admin_user = User(
+            username=username,
+            email=email,
+            hashed_password=hashed_password,
+            is_admin=True 
+        )
+
+        session.add(admin_user)
+        await session.commit()
+        print(f"Created admin user {username}, {email}")
