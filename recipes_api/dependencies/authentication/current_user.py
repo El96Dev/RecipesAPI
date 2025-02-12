975fd3bd301990183b2e2ddb042be44f9d189399
El96Dev <el96cpp@gmail.com>
2025-02-12 14:41:23 +0300

    Add current_admin_user dependency function

--- recipes_api/dependencies/authentication/current_user.py
+++ recipes_api/dependencies/authentication/current_user.py
@@ -1,3 +1,5 @@
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
 from fastapi_users import FastAPIUsers
 
 from core.models import User
@@ -11,3 +13,9 @@ fastapi_users = FastAPIUsers[User, UserIdType](
 )
 
 current_active_user = fastapi_users.current_user(active=True)
+
+
+async def current_admin_user(current_user: User = Depends(current_active_user)) -> User:
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User doesn't have admin privileges")
+    return current_user
